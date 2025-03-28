13fef87b35dbf6a59de658cb174d131a8e9c67c8
hmmasudsarder <hmmasudsardar@gmail.com>
2025-03-28 16:20:07 +0600

    added for loop print sum of the input  number 1 to 100

--- Module-3/sum_from_1_to_n.c
+++ Module-3/sum_from_1_to_n.c
@@ -2,6 +2,13 @@
 int main()
 {
     int n;
-    screnf("%d", &n); 
+    scanf("%d", &n); 
+    int sum = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        sum += i;
+    }
+    printf("%d\n", sum);
+    
     return 0;
 }
