5bb0a1112dba52803c9d49b6cdda994e0c4c5de7
hmmasudsarder <hmmasudsardar@gmail.com>
2025-03-30 19:16:09 +0600

    added for loop print sum of the input  number 1 to 100

--- Module-3/loop_condition.c
+++ Module-3/loop_condition.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 int main()
 {
-    for (int i =0 ; i<= 10; i++){
-        if (i%2 ==0){
-            printf("%d event\n", i);
-        }else{
-            printf("%d odd\n", i);
-        } 
+    for (int j = 5; j <= 10; j++)
+    {
+        printf("hi ");
     }
-    return 0;
 }
