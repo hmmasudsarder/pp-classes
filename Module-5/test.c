125754a984cc8390ad6f0c6c70c98068909f8d86
hmmasudsarder <hmmasudsardar@gmail.com>
2025-04-14 01:44:27 +0600

    added event to positive or nagetive or odd number problem solved

---
+++ Module-5/test.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main (){
+    int x = 5;
+    int y = 10;
+    if (x>=y && x <y +4)
+    {
+        if (y<5)
+        {
+            printf("hi");
+        }if (y<10)
+        {
+            printf("hi");
+        }
+        
+        
+        
+    }else
+    {
+        printf("Negative");
+    }
+    
+    
+    
+    return 0;
+}
