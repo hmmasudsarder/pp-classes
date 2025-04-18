efde4cf425afc5a5cfab5fff5a6055ec7d28530c
hmmasudsarder <hmmasudsardar@gmail.com>
2025-04-18 23:31:54 +0600

    reverse index print

---
+++ Conceptual/three/sum.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main (){
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    
+    int sum_pos=0, sum_neg=0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i]>0)
+        {
+            sum_pos+=arr[i];
+        }if (arr[i]<0)
+        {
+            sum_neg+=arr[i];
+        }
+        
+    }
+    printf("%d %d", sum_pos, sum_neg);
+    
+
+    return 0;
+}
