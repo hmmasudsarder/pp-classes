1e9a74ce8cf49383f431d1955d64ef45e7e42830
hmmasudsarder <hmmasudsardar@gmail.com>
2025-04-18 22:23:48 +0600

    problem poetiv or negetive added

---
+++ Conceptual/three/A_Ambitious_Kid.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main (){
+    int n;
+    scanf("%d", &n);
+    int min_num =100000+1;
+    for (int i = 1; i <=n; i++)
+    {
+        int x;
+        scanf("%d", &x);
+        if (x<0)
+        {
+            x=x*(-1);
+        }
+        
+        if (x< min_num)
+        {
+            min_num =x;
+        }
+        
+    }
+    printf("%d\n", min_num);
+    
+    return 0;
+}
