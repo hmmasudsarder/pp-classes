706c7a51742a18c8d6ee40e6cae87d39c5fb782f
hmmasudsarder <hmmasudsardar@gmail.com>
2025-05-03 23:29:38 +0600

    added new problem function in sum

--- Conceptual/Week4FirstSession/A_Add.c
+++ Conceptual/Week4FirstSession/A_Add.c
@@ -1,5 +1,16 @@
-#include<stdio.h>
-int main (){
-    
+#include <stdio.h>
+
+int sum_two_numbers(int x, int y) {
+    return x + y;
+}
+
+int main() {
+    int x, y;
+
+
+    scanf("%d %d", &x, &y);
+
+    printf("%d\n", sum_two_numbers(x, y));
+
     return 0;
 }
