2c9b5eead47998ec2d66d6813703217775a6b571
hmmasudsarder <hmmasudsardar@gmail.com>
2025-05-05 22:32:44 +0600

    added positive number to nagative number

--- Conceptual/Week4FirstSession/two_problem.c
+++ Conceptual/Week4FirstSession/two_problem.c
@@ -1,5 +1,19 @@
 #include<stdio.h>
+
+void positive_to_negative(int n){
+    for (int i = n; i >= -n; i--)
+    {
+        printf("%d ", i);
+    }
+    
+}
+
 int main (){
     int n; scanf("%d", &n);
+    if (n>0)
+    {
+        positive_to_negative(n);
+    }
+    
     return 0;
 }
