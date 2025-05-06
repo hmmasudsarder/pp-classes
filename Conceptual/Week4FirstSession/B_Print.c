f2ec95043cfba888baf5cc1b9f59dc11e2ff0b6e
hmmasudsarder <hmmasudsardar@gmail.com>
2025-05-06 23:44:50 +0600

    added new problem function n eqeal prient 1 to n

---
+++ Conceptual/Week4FirstSession/B_Print.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void print_numbers(int n) {
+    for (int i = 1; i <= n; i++) {
+        if (i > 1) {
+            printf(" "); 
+        }
+        printf("%d", i);
+    }
+    printf("\n"); 
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    print_numbers(n); 
+    return 0;
+}
