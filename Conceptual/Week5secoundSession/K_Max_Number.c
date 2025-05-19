c104c34b9785a47ccf9f580837f713d359f71e8d
hmmasudsarder <hmmasudsardar@gmail.com>
2025-05-19 16:47:54 +0600

    metrix array show in last colum or last row

--- Conceptual/Week5secoundSession/K_Max_Number.c
+++ Conceptual/Week5secoundSession/K_Max_Number.c
@@ -1,5 +1,31 @@
 #include<stdio.h>
+int findMax(int a[], int n, int inx){
+    if (inx == n)
+    {
+        return a[inx - 1];
+    }
+    int max = findMax(a, n, inx +1);
+    if (a[inx] > max)
+    {
+        a[inx];
+    }else
+    {
+        return max;
+    }
+    
+    
+    
+}
 int main (){
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    int max_value = findMax(a, n, 0);
+    printf("%d\n", max_value);
     
     return 0;
 }
